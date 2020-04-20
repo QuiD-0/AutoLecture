a51e08e6b9f8b19681a5c44d15bce3fd22965d28
QuiD <wodnd101@naver.com>
2020-04-20 20:46:37 +0900

    ID,PW입력 추가/원하는 강의만 수강 가능
    ID,PW입력 추가
    원하는 강의만 수강 가능
    exe 파일 추가

--- 자동강의수강.py
+++ 자동강의수강.py
@@ -1,19 +1,9 @@
 from selenium import webdriver
 import time
 
-path ="C:/chromedriver.exe" #C최상위에 파일 위치
-driver = webdriver.Chrome(path)
-driver.get('https://ecampus.ut.ac.kr/main/MainView.dunet')#학교 홈페이지로이동
-login=driver.find_element_by_name("id")
-login.send_keys("My-ID")
-login=driver.find_element_by_name("pass")
-login.send_keys("My-PW")
-driver.find_element_by_class_name("btn_login").click()#로그인
-time.sleep(7)#로그인 로딩시간 대기
-lec=len(driver.find_elements_by_xpath('/html/body/div[7]/div[3]/div/div[3]/div[2]/ul/li')) #총 강의 개수 확인
-for j in range (1,lec+1):#강의개수만큼 반복 
+def playlec(times):
     driver.get('https://ecampus.ut.ac.kr/lms/myLecture/doListView.dunet?mnid='+'201008840728')
-    driver.find_element_by_xpath("/html/body/div[7]/div[3]/div/div[3]/div[2]/ul/li["+str(j)+"]/div[1]/a").click();#j번째 과목 클릭
+    driver.find_element_by_xpath("/html/body/div[7]/div[3]/div/div[3]/div[2]/ul/li["+str(times)+"]/div[1]/a").click();#j번째 과목 클릭
     count=len(driver.find_elements_by_xpath('//*[@id="lenAct"]/div'))#해당 과목의 강의 개수 체크 
     for i in range(1,count+1):
         checkTerm=driver.find_element_by_xpath('//*[@id="lenAct"]/div['+str(i)+']/div[3]')#수강 기간
@@ -29,5 +19,27 @@ for j in range (1,lec+1):#강의개수만큼 반복
                 time.sleep(play*60+10)#sleep 1은 1초.플레이시간*60초 + 여분의 10초
                 print(str(play)+"분 실행후 종료 됩니다.")
     time.sleep(2)
-driver.close()
+
+path ="./chromedriver.exe" #파이썬/exe 파일 위치에 같이 있어야 한다.
+driver = webdriver.Chrome(path)
+driver.get('https://ecampus.ut.ac.kr/main/MainView.dunet')#학교 홈페이지로이동
+login=driver.find_element_by_name("id")
+login.send_keys(input("ID입력 : "))
+login=driver.find_element_by_name("pass")
+login.send_keys(input("PW입력 : "))
+driver.find_element_by_class_name("btn_login").click()#로그인
+time.sleep(7)#로그인 로딩시간 대기
+n = input("1:전부 수강 2:선택 수강")
+lec=len(driver.find_elements_by_xpath('/html/body/div[7]/div[3]/div/div[3]/div[2]/ul/li')) #총 강의 개수 확인
+if(n=="1"):
+    for j in range (1,lec+1):#강의개수만큼 반복 
+        playlec(j)
+    driver.close()
+if(n=="2"):
+    M = input("원하는 수강 번호를 입력하세요. 1~"+str(lec)+": 해당 강의 수강  0: 종료 ")
+    while(M!="0"): #M이 0이면 종료
+        playlec(M)
+        M = input("원하는 수강 번호를 입력하세요. 1~"+str(lec)+": 해당 강의 수강  0: 종료 ")
+    driver.close()
+
 print("학습 종료")
