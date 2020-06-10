26d7286b2a95d3cf2699a45da2e225ac8a8b0b17
QuiD <wodnd101@naver.com>
2020-06-11 00:08:12 +0900

    원하는 강의 수강
    원하는 강의 하나만 듣기 추가.

--- 자동강의수강.py
+++ 자동강의수강.py
@@ -1,23 +1,30 @@
 from selenium import webdriver
 import time
 
+def playvid(i):
+    checkTerm=driver.find_element_by_xpath('//*[@id="lenAct"]/div['+str(i)+']/div[3]')#수강 기간
+    checkPercent=driver.find_element_by_xpath('//*[@id="lenAct"]/div['+str(i)+']/div[2]/dl/dd[1]/span[2]')#수강 진행율
+    if(checkPercent.text!=" 100%"):#이미 수강완료 했다면  수강하지않는다.
+        if(checkTerm.text!="[학습시작전]"):#수강 가능날짜가 되지않았으면 수강하지않는다.
+            playtime=driver.find_element_by_xpath('//*[@id="lenAct"]/div['+str(i)+']/div[2]/dl/dd[1]') #들으려는 강의 총 수강시간 확인
+            playtime=playtime.text.split('/')
+            playtime=playtime[2].split('분')
+            driver.find_elements_by_xpath('//*[@id="lenAct"]/div['+str(i)+']/div[3]/a')[0].click()
+            checkPercent=checkPercent.text.split('%')#수강시간이 1%~99% 사이일 경우 시간 측정 
+            play=int(playtime[0])-int(checkPercent[0])*int(playtime[0])/100
+            print(str(round(play,1))+"분 실행후 종료 됩니다.")
+            time.sleep(play*60+10)#sleep 1은 1초.플레이시간*60초 + 여분의 10초
+
 def playlec(times):
     driver.get('https://ecampus.ut.ac.kr/lms/myLecture/doListView.dunet?mnid='+'201008840728')
     driver.find_element_by_xpath("/html/body/div[7]/div[3]/div/div[3]/div[2]/ul/li["+str(times)+"]/div[1]/a").click();#j번째 과목 클릭
     count=len(driver.find_elements_by_xpath('//*[@id="lenAct"]/div'))#해당 과목의 강의 개수 체크 
-    for i in range(1,count+1):
-        checkTerm=driver.find_element_by_xpath('//*[@id="lenAct"]/div['+str(i)+']/div[3]')#수강 기간
-        checkPercent=driver.find_element_by_xpath('//*[@id="lenAct"]/div['+str(i)+']/div[2]/dl/dd[1]/span[2]')#수강 진행율
-        if(checkPercent.text!=" 100%"):#이미 수강완료 했다면  수강하지않는다.
-            if(checkTerm.text!="[학습시작전]"):#수강 가능날짜가 되지않았으면 수강하지않는다.
-                playtime=driver.find_element_by_xpath('//*[@id="lenAct"]/div['+str(i)+']/div[2]/dl/dd[1]') #들으려는 강의 총 수강시간 확인
-                playtime=playtime.text.split('/')
-                playtime=playtime[2].split('분')
-                driver.find_elements_by_xpath('//*[@id="lenAct"]/div['+str(i)+']/div[3]/a')[0].click();
-                checkPercent=checkPercent.text.split('%')#수강시간이 1%~99% 사이일 경우 시간 측정 
-                play=int(playtime[0])-int(checkPercent[0])*int(playtime[0])/100
-                print(str(play)+"분 실행후 종료 됩니다.")
-                time.sleep(play*60+10)#sleep 1은 1초.플레이시간*60초 + 여분의 10초
+    i = input("1~"+str(count)+"중 입력, 0:종료 10:전부")
+    if(i!="0" and i!=10):
+        playvid(i)
+    if(i==10):
+        for i in range (1,count+1):
+            playvid(i)
     time.sleep(2)
 
 path ="./chromedriver.exe" #파이썬/exe 파일 위치에 같이 있어야 한다.
@@ -43,3 +50,4 @@ if(n=="2"):
     driver.close()
 
 print("학습 종료")
+
